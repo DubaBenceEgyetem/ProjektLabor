999c6d5267dae642692474c617c73017018171af
Duba Bence <duba.bence22@gmail.com>
2023-11-04 19:02:54 +0100

    #

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './LoginPage.css';
 
 function LoginPage()
 {
     return(
-        <div className='LoginPageWrapper' id='LoginForm'>
-            <form className='LoginPageInputHolder'>
-                <h1>Belépés</h1><br></br>
-                <input type='email' className='InputHolder' placeholder='Email cím' id='email'></input><br></br>
-                <input type='password' className='InputHolder' placeholder='Jelszó' id='password'></input><br></br>
-               <button type='submit'>Belépés</button><br></br>
-               <a href='/forgot' id='forgot'>Elfelejtetted a jelszavad?</a><br></br>
-               <a href='/register' id='register'>Regisztráció</a>     
-            </form>
-        </div>
+        <Fragment>           
+            <div className='LoginPageWrapper' id='LoginForm'>
+                <form className='LoginPageInputHolder'>
+                    <h1>Belépés</h1><br></br>
+                    <input type='email' className='InputHolder' placeholder='Email cím' id='email'></input><br></br>
+                    <input type='password' className='InputHolder' placeholder='Jelszó' id='password'></input><br></br>
+                <button type='submit'>Belépés</button><br></br>
+                <a href='/forgot' id='forgot'>Elfelejtetted a jelszavad?</a><br></br>
+                <a href='/register' id='register'>Regisztráció</a>     
+                </form>
+            </div>
+        </Fragment>
     );
 
 }
--- src/RegisterPage/RegisterPage.js
+++ src/RegisterPage/RegisterPage.js
@@ -1,21 +1,85 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import './RegisterPage.css';
+import axios from 'axios';
+
+
 
 function RegisterPage()
 {
+    const [name1, setKereszt] = useState('');
+    const [name2, setVezet] = useState('');
+    const [check, setSzamla] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setTelefon] = useState('');
+    const [password, setJelszo] = useState('');
+
+
+    const handleName1Change = (value) => 
+    {
+        setKereszt(value);
+    }
+    
+    const handleName2Change = (value) => 
+    {
+        setVezet(value);
+    }
+
+    
+    const handlePhoneChange = (value) => 
+    {
+        setTelefon(value);
+    }
+    
+    const handleEmailChange = (value) => 
+    {
+        setEmail(value);
+    }
+    
+    const handlePasswordChange = (value) => 
+    {
+        setJelszo(value);
+    }
+
+    const handleSave = () =>
+    {
+        const data = {
+            Kereszt_nev : name1,
+            Vezetek_nev : name2,
+            email : email,
+            Phone : phone,
+            Jelszo : password,
+            PhoneNo : phone
+                                //ÁDÁM ELNEVEZÉSEI KELLENEK
+        }
+        const url = 'https://localhost:44310/api/Test/Registration';
+        axios.post(url,data).then((result) =>{
+            alert(result.data);
+        })
+        .catch((error)=>
+            {
+                alert(error)
+            }
+        );
+    }
+
+
+
     return(
-        <div className='RegisterPageWrapper' id='RegisterForm'>
-            <div className='RegisterPageInputHolder'>
-                <h1>Regisztráció</h1><br></br>
-                <input type='text'placeholder='Keresztnév' id='kereszt'></input>
-                <input type='text'placeholder='Vezetéknév' id='vezet'></input>
-                <input type='text' className='InputHolder' placeholder='Számlaszám'></input>
-              <input type='email' className='InputHolder' placeholder='Email cím' id='email'></input><br></br>
-              <input type='password' className='InputHolder' placeholder='Jelszó' id='password'></input><br></br>
-               <button type='submit'>Regisztrálás</button><br></br>
-               <a href='/'>Belépés</a>         
+        <Fragment>
+            <div className='RegisterPageWrapper' id='RegisterForm'>
+                <div className='RegisterPageInputHolder'>
+                    <h1>Regisztráció</h1><br></br>
+                    <input type='text' placeholder='Keresztnév' id='kereszt' onChange={(e) => handleName1Change(e.target.value)}></input>
+                    <input type='text' placeholder='Vezetéknév' id='vezet' onChange={(e) => handleName2Change(e.target.value)}></input>
+                    <input type='text' className='InputHolder' placeholder='Számlaszám'></input>
+                <input type='email' className='InputHolder' placeholder='Email cím' id='email' onChange={(e) => handleEmailChange(e.target.value)}></input><br></br>
+                <input type='text' className='InputHolder' placeholder='Telefonszám' id='phone' onChange={(e) => handlePhoneChange(e.target.value)}></input><br></br>
+                <input type='password' className='InputHolder' placeholder='Jelszó' id='password' onChange={(e) => handlePasswordChange(e.target.value)}></input><br></br>
+                <button type='submit' onClick={() => handleSave()}>Regisztrálás</button><br></br>
+                <a href='/'>Belépés</a>         
+                </div>
             </div>
-        </div>
+        </Fragment>
     );
 
 }
