98f7081de1fef31726a68b2a3569b873d8ca4c26
Duba Bence <duba.bence22@gmail.com>
2023-11-15 17:40:18 +0100

    #

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -3,76 +3,82 @@ import './LoginPage.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
-function LoginPage()
-{
+function LoginPage() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
-    const [password, setJelszo] = useState('');
-
-
-    
-    const handlePasswordChange = (value) => 
-    {
-        setJelszo(value);
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+   
+    const handlePasswordChange = (value) => {
+        setPassword(value);
+        setPasswordError(false); 
     }
-    
-    const handleEmailChange = (value) => 
-    {
+
+    const handleEmailChange = (value) => {
         setEmail(value);
+        setEmailError(false); 
     }
-  
 
-    const onSubmit = (e) =>
-    {
-        e.preventDefault() 
+
+    const onSubmit = (e) => {
+        e.preventDefault();
         const data = {
-            Email : email,
-            Jelszo : password,
-                                //ÁDÁM ELNEVEZÉSEI KELLENEK
+            Email: email,
+            Jelszo: password,
+            
         }
         const url = 'https://localhost:44310/api/Test/Login';
-        axios.post(url,data).then((result) =>{
-                if(email.trim()  === "" || password.trim() === "")
-                    {
-                        document.getElementById("ErrorLabel2").innerHTML = "Töltse ki a hiányzó mezőket*"
-                    }
-                else if(result.data == 'User is valid')
-                    {
-                        navigate('/UserAccount')
-                    }
-                else
-                    {
-                      //  alert(result.data)
-                        document.getElementById("ErrorLabel2").innerHTML = "Hibás email vagy jelszó*";
-                    }
-        }
-        )
-        .catch((error)=>
-            {
-            
-                console.error(error); // Loggoljuk a hibát a konzolra
-                alert(error.message);
-            }
-        );
-    }
- 
+        axios.post(url, data)
+            .then((result) => {
+                if (email.trim() === "" || password.trim() === "") {
+                    document.getElementById("ErrorLabel2").innerHTML = "Töltse ki a hiányzó mezőket*";
 
-    return(
-        <Fragment>           
+                    setEmailError(email.trim() === "");
+                    setPasswordError(password.trim() === "");
+
+                } else if (result.data === 'User is valid') {
+                    console.log(data)
+                    navigate('/UserAccount')
+                } else {
+                    document.getElementById("ErrorLabel2").innerHTML = "Hibás email vagy jelszó*";
+                    
+                    setEmailError(true);
+                    setPasswordError(true);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                alert(error.message);
+            });
+    }
+
+    return (
+        <Fragment>
             <div className='LoginPageWrapper' id='LoginForm'>
                 <form className='LoginPageInputHolder' onSubmit={onSubmit}>
                     <h1>Belépés</h1><br></br>
-                    <input type='email' className='InputHolder' placeholder='Email cím' id='email' onChange={(e) => handleEmailChange(e.target.value)}></input><br></br>
-                    <input type='password' className='InputHolder' placeholder='Jelszó' id='password' onChange={(e) => handlePasswordChange(e.target.value)}></input><br></br>
-                    <div id='ErrorLabel2' className='ErrorLabel2'></div>
-                 <button type='submit'>Belépés</button><br></br>
-                <a href='/forgot' id='forgot'>Elfelejtetted a jelszavad?</a><br></br>
-                <a href='/register' id='register'>Regisztráció</a>     
+                    <input
+                        type='email'
+                        className={`InputHolder ${emailError ? 'error' : ''}`}
+                        placeholder='Email cím'
+                        id='email'
+                        onChange={(e) => handleEmailChange(e.target.value)}
+                    ></input><br></br>
+                    <input
+                        type='password'
+                        className={`InputHolder ${passwordError ? 'error' : ''}`}
+                        placeholder='Jelszó'
+                        id='password'
+                        onChange={(e) => handlePasswordChange(e.target.value)}
+                    ></input><br></br>
+                    <span id='ErrorLabel2' className='ErrorLabel2'></span>
+                    <button type='submit'>Belépés</button><br></br>
+                    <a href='/forgot' id='forgot'>Elfelejtetted a jelszavad?</a><br></br>
+                    <a href='/register' id='register'>Regisztráció</a>
                 </form>
             </div>
         </Fragment>
     );
-
 }
 export default LoginPage;
